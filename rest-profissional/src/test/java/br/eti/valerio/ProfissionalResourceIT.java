11e86bfc7157ec5a1cc9e232a9ec4c0d25a7fbb6
Hamilton Valério Filho <h.valerio.filho@avanade.com>
2022-09-19 18:00:53 -0300

    Novos microsserviços

---
+++ rest-profissional/src/test/java/br/eti/valerio/ProfissionalResourceIT.java
@@ -0,0 +1,8 @@
+package br.eti.valerio;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ProfissionalResourceIT extends ProfissionalResourceTest {
+    // Execute the same tests but in packaged mode.
+}
