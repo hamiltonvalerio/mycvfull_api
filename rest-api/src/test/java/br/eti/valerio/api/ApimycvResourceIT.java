0f6b96ac17e6b3e814cbc5f28421695608516e6e
Hamilton Valério Filho <h.valerio.filho@avanade.com>
2022-09-20 09:16:05 -0300

    Mais ajustes

--- rest-api/src/test/java/br/eti/valerio/api/ApimycvResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.eti.valerio.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ApimycvResourceIT extends ApimycvResourceTest {
-
-    // Execute the same tests but in native mode.
-}
